91aa684f8e51a01be577716d288e1a456eff4a5e
Robson <jrobson.santos81@gmail.com>
2024-06-14 19:40:03 -0300

    feat: cria funcionalidades para o botão de adicionar nome

---
+++ script.js
@@ -0,0 +1,27 @@
+let amigos = [];
+
+const txtAmigo = document.querySelector('.input-amigo');
+const btnAdicionar = document.querySelector('.add');
+const listaParticipantes = document.querySelector('.participantes');
+const mensagemErro = document.querySelector('.mensagem-erro');
+
+
+btnAdicionar.addEventListener('click', () => {
+
+    if (txtAmigo.value.length === 0 ) {
+        mensagemErro.textContent = "ERRO! Insira um nome."
+    } else if (amigos.includes(txtAmigo.value)) {
+        mensagemErro.textContent = 'ERRO! Nome já inserido';
+    } else { mensagemErro.textContent = '';
+    if (listaParticipantes.textContent.length === 0 ) {
+        listaParticipantes.textContent = txtAmigo.value;
+    } 
+    /*else {
+        listaParticipantes.textContent += ", " + txtAmigo.value;
+    }*/
+    amigos.push(txtAmigo.value); 
+    listaParticipantes.textContent = amigos.join(', ');
+    txtAmigo.value = '';
+}
+
+})
