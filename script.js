8988f834bd8db3b7bf89d2a8f5c626f18ec90762
Robson <jrobson.santos81@gmail.com>
2024-06-20 21:41:11 -0300

    feat: cria a funcionalidade que limpa os nomes sorteados

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const btnAdicionar = document.querySelector('.add');
 const listaParticipantes = document.querySelector('.participantes');
 const mensagemErro = document.querySelector('.mensagem-erro');
 const btnReiniciar = document.querySelector('.reset');
+const btnSortear = document.querySelector('.sort')
+const sorteio = document.querySelector('.sorteio')
 
 btnAdicionar.addEventListener('click', () => {
 
@@ -26,4 +28,35 @@ btnReiniciar.addEventListener('click', () => {
     listaParticipantes.textContent = '';
     mensagemErro.textContent = '';
     txtAmigo.value = '';
+    sorteio.innerHTML='';
 });
+
+btnSortear.addEventListener('click', () => {
+    sortear()
+});
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function sortear() {
+     if (amigos.length < 4) {
+         alert('Adicone ao menos 4 amigos');
+    return;
+}
+    embaralhar(amigos);
+
+    let sorteio = document.querySelector('.sorteio');
+    sorteio.innerHTML='';
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + '<li>' + amigos[i] + '-->' + amigos[0] + '</li>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + '<li>' + amigos[i] + '-->' + amigos[i+ 1] + '</li>';
+        }
+}
+}
+
